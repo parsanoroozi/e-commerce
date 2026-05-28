086801eb8338947df490c7ec6867ca7c6ce9b623
parsanoroozi <parsan6347@gmail.com>
2026-05-28 22:31:19 +0330

    one more change

--- frontend/e-commerce-frontend/vite.config.js
+++ frontend/e-commerce-frontend/vite.config.js
@@ -7,11 +7,11 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:8080',
+        target: 'http://192.168.1.100:8080',
         changeOrigin: true,
       },
       '/uploads': {
-        target: 'http://localhost:8080',
+        target: 'http://192.168.1.100:8080',
         changeOrigin: true,
       },
     },
