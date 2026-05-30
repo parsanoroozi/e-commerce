25595d28e3d1d7dcfcae75e2cd53408656a8dbaf
parsanoroozi <parsan6347@gmail.com>
2026-05-30 10:28:13 +0330

    small debug

--- frontend/e-commerce-frontend/src/api/client.js
+++ frontend/e-commerce-frontend/src/api/client.js
@@ -44,7 +44,7 @@ export async function apiRequest(path, options = {}) {
     const message = await parseError(response);
     throw new ApiError(response.status, message);
   }
-  
+
   return response.json();
 }
 
